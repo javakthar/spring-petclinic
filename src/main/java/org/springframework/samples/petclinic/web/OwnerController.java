a63d8780221f21ba4f1b891905f62019e944aa39
javed.khan <javed.khan@BRILLIO.COM>
2016-05-16 20:10:41 +0530

    BD-233 #time 1h #comment Job complete can be deployed

--- src/main/java/org/springframework/samples/petclinic/web/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/web/OwnerController.java
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * Added few lines of code
+ * Added some code
  */
 package org.springframework.samples.petclinic.web;
 
